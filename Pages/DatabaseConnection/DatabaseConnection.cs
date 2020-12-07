483c3f4cc12c6b4dd80514956f26adbfb21c55a8
Sara <sara1999200407@gmail.com>
2020-12-07 10:57:40 +0000

    changes for the contact page

---
+++ Pages/DatabaseConnection/DatabaseConnection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Group_Project.Pages.DatabaseConnection
+{
+    public class DatabaseConnection
+    {
+
+        public string DatabaseString()
+        {
+
+            string DBString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Sara\source\repos\Group_Project\Databases\Database.mdf;Integrated Security=True";
+            return DBString;
+        }
+    }
+}
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,6 +2,35 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+function validate(); {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+    var error_message = document.getElementById("error_message");
+    var text 
+
+    error_message.style.padding = "10px";
+
+    var text;
+    if (name.length < 5) {
+        text = "Please Enter valid Name";
+        error_message.innerHTML = text;
+        return false;
+    }
+    }
+    if (email.indexOf("@") == -1 || email.length < 6) {
+        text = "Please Enter valid Email";
+        error_message.innerHTML = text;
+        return false;
+    }
+    if (message.length <= 100) {
+        text = "Please Enter More Than 100 Characters";
+        error_message.innerHTML = text;
+        return false;
+    }
+{alert("Form Submitted Successfully!");
+    return true;
+}
 
 function readURL(input) {
     var reader = new FileReader();
